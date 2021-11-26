ecab932c3ec81409dca8e0819c075c4c71c8c2a8
Maura Mathieu <mmaura@amc-i.fr>
2021-11-26 18:50:18 +0100

    .

--- .history/app/App_20211126184345.ts
+++ .history/app/App_20211126184345.ts
@@ -6,7 +6,7 @@ import * as path from 'path'
 class App {
   gems: Array<any> = []
 
-  async run() {
+  async run () {
     console.log('Running')
 
     this.gems = [
@@ -26,7 +26,7 @@ class App {
       console.log('\n\n' + this.gems.length + ' gems processed and written to file')
     }
 
-
+    
   }
 
   /**
@@ -35,7 +35,7 @@ class App {
    * @param limit 
    * @param offset 
    */
-  async processGemsVendorChunk(api: String, limit: Number, offset: Number): Promise<Array<any>> {
+  async processGemsVendorChunk (api: String, limit: Number, offset: Number): Promise<Array<any>> {
     console.log('Calling API for gems with vendor rewards chunk of data')
     let response = await axios.get(POEWikiApi.getChunkAPI(api, limit, offset))
     console.log(POEWikiApi.getChunkAPI(api, limit, offset))
@@ -56,7 +56,7 @@ class App {
           npc: gem.npc,
           quest: gem.quest,
           act: gem.act,
-          classes: ((gem.classes) && gem.classes.length > 0) ? gem.classes.split('�') : []
+          classes: gem.classes.length > 0 ? gem.classes.split('�') : []
         })
       } else {
         // If it's a new gem, create an entry in `gems` object for it
@@ -64,11 +64,11 @@ class App {
           name: gem.name,
           required_level: gem['required level'],
           primary_attr: gem['primary attribute'],
-          vendor_rewards: [{
+          vendor_rewards: [{ 
             npc: gem.npc,
             quest: gem.quest,
             act: gem.act,
-            classes: ((gem.classes) && gem.classes.length > 0) ? gem.classes.split('�') : []
+            classes: ((gem.classes)&&gem.classes.length > 0) ? gem.classes.split('�') : []
           }],
           quest_rewards: []
         })
@@ -85,7 +85,7 @@ class App {
    * @param limit 
    * @param offset 
    */
-  async processGemsQuestChunk(api: String, limit: Number, offset: Number) {
+  async processGemsQuestChunk (api: String, limit: Number, offset: Number) {
     console.log('Calling API for gems with quest rewards chunk of data')
     let response = await axios.get(POEWikiApi.getChunkAPI(api, limit, offset))
     let data = response.data.cargoquery
@@ -99,11 +99,7 @@ class App {
         throw new Error('Something gone really wrong, the gems from API seems to not be synchoronized!')
       }
 
-      if (
-        ((gem.quest) && gem.quest.length > 0)
-        || ((gem.quest) && gem.act.length > 0)
-        || ((gem.quest) && gem.classes.length > 0)
-      ) {
+      if (gem.quest.length > 0 || gem.act.length > 0 || gem.classes.length > 0) {
         existingGem.quest_rewards.push({
           quest: gem.quest,
           act: gem.act,
@@ -117,7 +113,7 @@ class App {
    * Checks if there is no duplicates in the gems processed from the API.
    * Because we are loading the data in chunks there may be a situation when one gem occure in both chunks.
    */
-  async assertNoDuplicates(): Promise<Boolean> {
+  async assertNoDuplicates (): Promise<Boolean> {
     if (this.gems.length === 0) {
       console.log('Gems are not loaded in, there is nothing to check, did you forget to call `run()` method?')
       return
@@ -127,19 +123,19 @@ class App {
     let map = {};
 
     for (var i = 0; i < this.gems.length; i++) {
-      var index = JSON.stringify(this.gems[i].name);
-      if (!map[index]) {
-        map[index] = 1;
-      } else {
-        map[index]++;
-      }
+        var index = JSON.stringify(this.gems[i].name);
+        if (!map[index]) {
+            map[index] = 1;
+        } else {
+            map[index]++;
+        }
     }
 
     for (var key in map) {
-      if (map[key] > 1) {
-        result = false
-        console.log(JSON.parse(key) + 'is duplicated, found: ' + map[key] + ' entries');
-      }
+        if (map[key] > 1) {
+          result = false
+          console.log (JSON.parse(key) + 'is duplicated, found: ' + map[key] + ' entries');
+        }
     }
 
     return result
